7478bfe945aa99199ead6af0e754b7bb51ce3300
Chris McMahon <cmcmahon@goodlabs.studio>
2025-11-07 12:21:19 -0500

    Add discover_files_with_substring_in_name() to file_tools.py

--- cmmc/file_tools.py
+++ cmmc/file_tools.py
@@ -17,6 +17,22 @@ def discover_files_with_name(name: str, dir: Path) -> List[Path]:
     return list(dir.rglob(name))
 
 
+def discover_files_with_substring_in_name(substring: str, dir: Path) -> List[Path]:
+    """
+    Recursively find all files with the substring in their name (ex. "foo" in
+    "xxxfooxx.bar") under the given directory.
+    """
+    if not isinstance(substring, str):
+        raise ValueError(
+            f"Expecting string but received {type(substring)}: {substring}"
+        )
+
+    if not isinstance(dir, Path):
+        raise ValueError(f"Expecting Path but received {type(dir)}: {dir}")
+
+    return [p for p in dir.rglob("*") if substring in p.name and p.is_file()]
+
+
 def load_ndjson(file_path: str | Path, encoding: str = "utf-8"):
     """
     Returns the contents of a newline delimited json file, with each line as an element in a list.
